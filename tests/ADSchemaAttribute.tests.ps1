077fe71bb7ed0a6df7c656bbeb74608163ab2512
robderickson <rob.derickson@gmail.com>
2020-04-24 00:50:42 -0400

    Mocked FindClassMandatoryProps and FindClassOptionaProps private functions.

--- tests/ADSchemaAttribute.tests.ps1
+++ tests/ADSchemaAttribute.tests.ps1
@@ -1,34 +1,34 @@
 InModuleScope ADSchema {
     Describe "ADSchema Attribute Functions" {
-        Context "Get-ADSchemaAttribute" {
-            $password = ConvertTo-SecureString "TestPassword" -AsPlainText -Force
-            $testcred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList ('testuser',$password)
+        $password = ConvertTo-SecureString "TestPassword" -AsPlainText -Force
+        $testcred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList ('testuser',$password)
 
-            Mock -ModuleName ADSchema FindClassMandatoryProps {} -Verifiable
-            Mock -ModuleName ADSchema FindClassOptionalProps {} -Verifiable
+        Mock -ModuleName ADSchema FindClassMandatoryProps {} -Verifiable
+        Mock -ModuleName ADSchema FindClassOptionalProps {} -Verifiable
+
+        It "Get-ADSchemaAttribute exists as a function in the module" {
+            (Get-Command Get-ADSchemaAttribute).count | should be 1
+        }
+
+        It "New-ADSchemaAttribute exists as a function in the module" {
+            (Get-Command New-ADSchemaAttribute).count | should be 1
             
-            $result = Get-ADSchemaAttribute -Class User -Attribute CN
+        }
 
-            It "exists as a function in the module" {
-                (Get-Command Get-ADSchemaAttribute).count | should be 1
-            }
+        Context "Get-ADSchemaAttribute without credentials" {            
+            $result = Get-ADSchemaAttribute -Class User -Attribute CN
 
             It "calls FindClassMandatoryProps and FindClassOptionalProps" {
                 Assert-VerifiableMock
             }
-
-            <# It "calls FindClassMandatoryProps and FindClassOptionalProps when ComputerName and Credential exist" {
-                Get-ADSchemaAttribute -Class User -Attribute CN -ComputerName dc -Credential $testcred
-                Assert-VerifiableMocks
-            }#>
         }
 
-        Context "New-ADSchemaAttribute" {
-            It "exists as a function in the module" {
-                (Get-Command New-ADSchemaAttribute).count | should be 1
-                
+        Context "Get-ADSchemaAttribute with ComputerName and Credential" {            
+            $result = Get-ADSchemaAttribute -Class User -Attribute CN -ComputerName dc -Credential $testcred
+
+            It "calls FindClassMandatoryProps and FindClassOptionalProps" {
+                Assert-VerifiableMock
             }
         }
-
     }
 }
